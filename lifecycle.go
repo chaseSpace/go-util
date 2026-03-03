a56b8419a21b038f1fd81ff02a486f08d2ed2509
lx <tmp@invalid.com>
2026-03-03 14:33:38 +0800

    optimize Listening()

--- lifecycle.go
+++ lifecycle.go
@@ -10,17 +10,20 @@ import (
 )
 
 // Listening 监听退出信号并执行优雅关闭
-func Listening(timeout time.Duration, onClose ...func(ctx context.Context)) {
+func Listening(timeout time.Duration, errs <-chan error, onClose ...func(ctx context.Context)) {
 	// 创建一个通道来接收退出信号
 	quit := make(chan os.Signal, 1)
 
 	// 监听指定的信号
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	// 等待接收退出信号
-	sig := <-quit
-
-	log.Printf("Received signal: %s, start graceful shutdown...", sig)
+	// 等待退出信号
+	select {
+	case err := <-errs:
+		log.Printf("Received listen err: %v\n", err)
+	case sig := <-quit:
+		log.Printf("Received signal: %s, start graceful shutdown...\n", sig)
+	}
 
 	// 创建带有超时的上下文
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
